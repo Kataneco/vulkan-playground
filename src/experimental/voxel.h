d815ec3db9039905fefcc1507cf07a561951ae73
Kataneco <null>
2025-02-18 23:03:23 +0100

    I really don't know what I'm doing TODO: Go back and rereview your shaders please

--- src/experimental/voxel.h
+++ src/experimental/voxel.h
@@ -2,9 +2,11 @@
 #include "util/VulkanUtils.h"
 
 struct Voxel {
-    uint32_t morton_code; //interleaved xyz
+    uint32_t morton; //interleaved xyz
+    uint32_t section; //extension
     uint32_t color; //4x8bit RGBA
-    uint64_t normal; //3x16bit unorm + 1x16bit flags
+    //uint64_t normal; //3x16bit unorm + 1x16bit flags
+    glm::vec3 normal;
 };
 
 struct OctreeNode {
